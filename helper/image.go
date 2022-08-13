5a9415238f81a73920d59dc8ed68cce93591b4ce
Pokin Chaitanasakul <pokin67980@gmail.com>
2022-08-13 14:19:34 +0700

    refactor helper package

--- helper/image.go
+++ helper/image.go
@@ -9,7 +9,6 @@ import (
 	"github.com/bosspokin/image-storer/dto"
 	"github.com/cloudinary/cloudinary-go"
 	"github.com/cloudinary/cloudinary-go/api/uploader"
-	"golang.org/x/crypto/bcrypt"
 )
 
 func UploadImage(file dto.File) (string, error) {
@@ -69,20 +68,4 @@ func DeleteImage(publicID string) error {
 	})
 
 	return err
-}
-
-func HashPassword(password string) (string, error) {
-	hash, err := bcrypt.GenerateFromPassword([]byte(password), bcrypt.DefaultCost)
-
-	if err != nil {
-		return "", err
-	}
-
-	return string(hash), nil
-}
-
-func CheckPasswordHash(password, hash string) bool {
-	err := bcrypt.CompareHashAndPassword([]byte(hash), []byte(password))
-
-	return err == nil
 }
